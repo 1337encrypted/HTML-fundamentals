919ffc689f3062b2687c2807b2d4b902c0a215e4
= <=>
2022-10-20 10:27:01 +0530

    Added tailwindcss

--- internship-projects/buttonclear/checkout.js
+++ internship-projects/buttonclear/checkout.js
@@ -1,5 +1,23 @@
 $(function(){
+    let firsttime = true;
+
+    $(".cardbox").hide();
     $("#confirm").on("click", function(){
-        $("#list").append("<li>"+$("#name").val()+" "+$("#address").val()+"<input type='radio' name='sameradiobtn'></li>")
+        if(firsttime){
+            firsttime=false;
+            $("#card1").show();
+            $("#list1").append("<li>"+$("#name").val()+" "+$("#address").val()+" "+$("#street").val()+" "+$("#city").val()+" "+$("#pincode").val()+" "+$("#mobile").val()+"</li>")
+            $("#name").val("");
+            $("#address").val("");
+            $("#street").val("");
+            $("#city").val("");
+            $("#pincode").val("");
+            $("#mobile").val("");
+        }
+        else{
+            $("#card2").show();
+            $("#list2").append("<li>"+$("#name").val()+" "+$("#address").val()+" "+$("#street").val()+" "+$("#city").val()+" "+$("#pincode").val()+" "+$("#mobile").val()+"</li>")
+            $("#name").val("");
+        }
     });
 });
